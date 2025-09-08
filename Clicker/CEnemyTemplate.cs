bf841ccc3b298a9c440f7c26e680c6ea57f405a5
Vladislav <vladotegov253@gmail.com>
2025-09-08 15:54:20 +0700

    Полностью Реализован класс CEnemyTemplate, частично реализован класс CEnemyTemplateList

---
+++ Clicker/CEnemyTemplate.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clicker
+{
+    class CEnemyTemplate
+    {
+        string name;
+        string iconName;
+
+        int baseLife;
+        double lifeModifier;
+
+        int baseGold;
+        double goldModifier;
+
+        double spawnChance;
+
+        public CEnemyTemplate(string name, string iconName, int baseLife, double lifeModifier, int baseGold, double goldModifier, double spawnChance)
+        {
+            this.name = name;
+            this.iconName = iconName;
+            this.baseLife = baseLife;
+            this.lifeModifier = lifeModifier;
+            this.baseGold = baseGold;
+            this.goldModifier = goldModifier;
+            this.spawnChance = spawnChance;
+        }
+
+        public string Name()
+        {
+            return name;
+        }
+        public string IconName()
+        {
+            return iconName;
+        }
+        public int BaseLife()
+        {
+            return baseLife;
+        }
+        public double LifeModifier()
+        {
+            return lifeModifier;
+        }
+        public int BaseGold()
+        {
+            return baseGold;
+        }
+
+        public double GoldModifier()
+        {
+            return goldModifier;
+        }
+        public double SpawnChance()
+        {
+            return spawnChance;
+        }
+    }
+}
---
+++ Clicker/CEnemyTemplateList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clicker
+{
+    class CEnemyTemplateList
+    {
+        List<CEnemyTemplate> enemies;
+        public CEnemyTemplateList()
+        {
+            enemies = new List<CEnemyTemplate>();
+        }
+        public void addEnemy(string name, string iconName, int baseLife, double lifeModifier, int baseGold, double goldModifier, double spawnChance)
+        {
+            enemies.Add(new CEnemyTemplate(name, iconName, baseLife, lifeModifier, baseGold, goldModifier, spawnChance));
+        }
+        public CEnemyTemplate getEnemyByName(string name)
+        {
+            foreach(CEnemyTemplate enemy in enemies)
+            {
+                if (enemy.Name() == name)
+                {
+                    return enemy;
+                }
+            }
+            return null;
+        }
+        public CEnemyTemplate getEnemyByInndex(int id)
+        {
+            if (id >=0 && id < enemies.Count)
+            {
+                return enemies[id];
+            }
+            return null;
+        }
+
+    }
+}
